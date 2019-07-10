b709860bf9c4244329b69c90298470a3e6c79eaa
chetan <chetankoranga3@gmail.com>
2019-07-10 23:26:51 +0530

    working on deleting button and favorite button

--- Firstweb/settings.py
+++ Firstweb/settings.py
@@ -74,15 +74,19 @@ WSGI_APPLICATION = 'Firstweb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'albummodel',
-        'HOST':'localhost',
-        'USER':'postgres',
-        'PASSWORD':'chetan123',
-    }
+         'default':{
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         }
+
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'albummodel',
+#         'HOST':'localhost',
+#         'USER':'postgres',
+#         'PASSWORD':'chetan123',
+#     }
 
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
